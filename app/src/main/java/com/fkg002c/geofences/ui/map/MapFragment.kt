b8eb2701a2ff04438ab7cc7be93f9b7e797052c2
Konstantin Grenaderov <fkg002c@gmail.com>
2025-10-21 22:22:22 +0300

    Geofences app initial version

---
+++ app/src/main/java/com/fkg002c/geofences/ui/map/MapFragment.kt
@@ -0,0 +1,50 @@
+package com.fkg002c.geofences.ui.map
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.fkg002c.geofences.R
+import com.fkg002c.geofences.databinding.FragmentMapBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MapFragment : Fragment() {
+
+    private var _binding: FragmentMapBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val mapViewModel =
+            ViewModelProvider(this).get(MapViewModel::class.java)
+
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+
+        val textView: TextView = binding.textMap
+        mapViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
